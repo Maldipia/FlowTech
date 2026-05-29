35371d521c8eee4d63b33218397e54998e07a7d2
Maldipia <tygfsb@gmail.com>
2026-05-29 11:43:22 +0800

    feat: robots.txt

---
+++ app/robots.js
@@ -0,0 +1,6 @@
+export default function robots() {
+  return {
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/admin', '/api'] }],
+    sitemap: 'https://www.flowtech.ph/sitemap.xml',
+  };
+}
